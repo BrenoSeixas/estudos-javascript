290576892243dd77bd46b7f5da690e1a59bc7cb8
Breno Seixas <olegarioseixasb@gmail.com>
2025-10-31 21:49:45 -0300

    Tipos de Dados

---
+++ tipos-de-dados/script.js
@@ -0,0 +1,30 @@
+var nome = "Breno";
+var sobrenome = "Seixas";
+console.log(typeof nome);
+
+var nomeCompleto = nome + " " + sobrenome;
+console.log(nomeCompleto);
+
+var gols = 1000;
+var frase =`Ele fez ${gols} nesse ano`;
+console.log(frase);
+
+// Declare uma variável contendo uma string
+var estado = "RJ";
+
+// Declare uma variável contendo um número dentro de uma string
+var  frase1 ="Ele fez isso " + 2 +" Vezes"; 
+
+// Declare uma variável com a sua idade
+var idade = 20;
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+
+// Coloque a seguinte frase em uma variável: It's time
+var time = "It's time";
+
+// Verifique o tipo da variável que contém o seu nome
+console.log(typeof nome);
+
+
