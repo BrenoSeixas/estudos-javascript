29eda1b4c9ce369abf2a9c3912acfa01bef248dc
Breno Seixas <olegarioseixasb@gmail.com>
2025-11-03 16:21:59 -0300

    Tudo é Objeto

---
+++ tudo-e-objeto/script.js
@@ -0,0 +1,12 @@
+var nome = "Breno";
+
+var nomeMinusculo = nome.toLowerCase();
+
+var btn = document.querySelector(".btn");
+
+// nomeie 3 propriedades ou métodos de strings
+
+// nomeie 5 propriedades ou métodos de elementos do DOM
+
+// busque na web um objeto (método) capaz de interagir com o clipboard, 
+// clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V
