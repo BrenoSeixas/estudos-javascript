fd913a09daace53f2fc2e019b01d93fe4dd16e11
Breno Seixas <olegarioseixasb@gmail.com>
2025-11-04 15:04:36 -0300

    Atribuição e Ternário

---
+++ atribuicao-e-ternario/script.js
@@ -0,0 +1,37 @@
+var n1 =  10;
+var n2 =  20;
+
+console.log(n1+=n2);
+
+
+var idade = 19
+var temDiabetes = false
+
+
+function g(teste1){
+return console.log(1);
+}
+
+function h(teste2){
+return console.log(2);
+}
+
+podeBeber = (idade >= 19 && temDiabetes)? g() : h();
+console.log(podeBeber);
+
+// Some 500 ao valor de scroll abaixo,
+// atribuindo o novo valor a scroll
+var n3 = 1000;
+n3 += 500;
+console.log(n3)
+
+// Atribua true para a variável darCredito,
+// caso o cliente possua carro e casa.
+// E false caso o contrário.
+var possuiCarro = true;
+var possuiCasa = true;
+var darCredito;
+
+
+darCredito = (possuiCarro && possuiCasa )? "Dar crédito": "Não dar Credito"
+console.log(darCredito);
