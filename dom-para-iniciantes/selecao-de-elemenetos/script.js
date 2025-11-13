273791458fffe8e633c329d42d9938c4fedb5975
Breno Seixas <olegarioseixasb@gmail.com>
2025-11-13 14:37:48 -0300

    inicio construtores

--- dom-para-iniciantes/selecao-de-elemenetos/script.js
+++
@@ -1,9 +0,0 @@
-const animais = document.getElementById("animais");
-//console.log(animais.innerText)
-
-const gridSection = document.getElementsByClassName("grid-section");
-console.log(gridSection)
-
-const primeiraLi = document.querySelector("li");
-console.log(primeiraLi);
-
