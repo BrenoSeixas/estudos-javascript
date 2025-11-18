7b4a1f47d90fce4d81cb3241408e4bd06bcc3dbf
Breno Seixas <olegarioseixasb@gmail.com>
2025-11-17 23:49:06 -0300

    String

---
+++ objetos/string/script.js
@@ -0,0 +1,86 @@
+// const comida = 'Pizza';
+// const agua = new String('Agua');
+
+// console.log(comida[3])
+// console.log(comida.length)
+// console.log(comida[comida.length - 1])
+
+// const linguagem = 'JavaScript'; 
+// console.log(linguagem.charAt(0))
+// console.log(linguagem.charAt(6))
+// console.log(linguagem.charAt(linguagem.length - 1))
+
+// const fruta = 'Banana';
+// const listaFrutas = 'Melancia, Banana, Laranja';
+
+// // console.log(listaFrutas.includes(fruta));
+// // console.log(fruta.includes(listaFrutas));
+
+// // console.log(fruta.startsWith('Ba'))
+// // console.log(fruta.endsWith('na'))
+
+
+// const transacao1 = 'Depósito de cliente';
+// const transacao2 = 'Depósito de fornecedor';
+// const transacao3 = 'Taxa de camisas';
+
+// transacao1.slice(0, 3); // Dep
+// transacao2.slice(0, 3); // Dep
+// transacao3.slice(0, 3); // Tax
+
+// transacao1.slice(12); // cliente
+// transacao1.slice(-4); // ente
+// transacao1.slice(3, 6); // ósi
+
+
+// console.log(fruta.indexOf('n'))
+
+const preco = "R$ 99,00"
+
+console.log(preco.padStart(15,'.'))
+console.log(preco.padEnd(15,'.'))
+
+// const frase = 'Ta';
+// console.log(frase.repeat(6))
+
+// // Utilizando o foreach na array abaixo,
+// // some os valores de Taxa e os valores de Recebimento
+
+// const transacoes = [
+//   {
+//     descricao: 'Taxa do Pão',
+//     valor: 'R$ 39',
+//   },
+//   {
+//     descricao: 'Taxa do Mercado',
+//     valor: 'R$ 129',
+//   },
+//   {
+//     descricao: 'Recebimento de Cliente',
+//     valor: 'R$ 99',
+//   },
+//   {
+//     descricao: 'Taxa do Banco',
+//     valor: 'R$ 129',
+//   },
+//   {
+//     descricao: 'Recebimento de Cliente',
+//     valor: 'R$ 49',
+//   },
+// ];
+
+// // Retorne uma array com a lista abaixo
+// const transportes = 'Carro;Avião;Trem;Ônibus;Bicicleta';
+
+// // Substitua todos os span's por a's
+// const html = `<ul>
+//                 <li><span>Sobre</span></li>
+//                 <li><span>Produtos</span></li>
+//                 <li><span>Contato</span></li>
+//               </ul>`;
+
+// // Retorne o último caracter da frase
+// const frase = 'Melhor do ano!';
+
+// // Retorne o total de taxas
+// const transacoes = ['Taxa do Banco', '   TAXA DO PÃO', '  taxa do mercado', 'depósito Bancário', 'TARIFA especial'];
