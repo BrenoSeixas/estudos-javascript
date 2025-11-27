ed6e3d928724bd764be36e8eb1c50b83edd72205
Breno Seixas <olegarioseixasb@gmail.com>
2025-11-27 18:59:22 -0300

    Modules

---
+++ efeitos-no-dom/modulo/js/modulos/teste.js
@@ -0,0 +1,9 @@
+export function teste1(){
+  console.log('teste')
+}
+
+export function teste2(){
+  console.log('teste2')
+}
+
+export const senha = "rerrerererffdfs";
---
+++ efeitos-no-dom/modulo/script.js
@@ -0,0 +1,13 @@
+import initScrollSuave from "./js/modulos/scroll-suave.js";
+import initAnimacaoScroll from "./js/modulos/animacao-scroll.js";
+import initAccordion from "./js/modulos/acordion.js";
+import initTabNav from "./js/modulos/anima-tab.js";
+import * as teste from "./js/modulos/teste.js";
+
+initScrollSuave();
+initAnimacaoScroll();
+initAccordion();
+initTabNav();
+teste.teste1();
+teste.teste2();
+console.log(teste.senha)
