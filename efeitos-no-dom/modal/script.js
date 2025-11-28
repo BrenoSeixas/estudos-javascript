2600d03891327cdda0a93fca14825d8a271bc1ad
Breno Seixas <olegarioseixasb@gmail.com>
2025-11-27 21:57:46 -0300

    Modal

---
+++ efeitos-no-dom/modal/script.js
@@ -0,0 +1,11 @@
+import initScrollSuave from "./js/modulos/scroll-suave.js";
+import initAnimacaoScroll from "./js/modulos/animacao-scroll.js";
+import initAccordion from "./js/modulos/acordion.js";
+import initTabNav from "./js/modulos/anima-tab.js";
+import initModal from "./js/modulos/modal.js";
+
+initScrollSuave();
+initAnimacaoScroll();
+initAccordion();
+initTabNav();
+initModal();
